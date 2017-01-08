32f0c7c913cf61a123584d11f169fb5239ed425a
Fredrik Jensen <fredrik@netliferesearch.com>
2017-01-08 23:24:19 +0100

    Cleaning up a bit :shower:

--- src/styles.js
+++ src/styles.js
@@ -5,21 +5,13 @@ import vars from './variables';
 
 injectGlobal`
   html {
-    /*
-     * Prevent certain mobile browsers from automatically zooming fonts
-     */
-    -webkit-text-size-adjust: 100%;
-        -ms-text-size-adjust: 100%;
-    /*
-     * fonts on OSX will look more consistent with other systems that do not
-     * render text using sub-pixel anti-aliasing.
-     */
+    text-size-adjust: 100%;
     -moz-osx-font-smoothing: grayscale;
     -webkit-font-smoothing: antialiased;
   }
   body {
-    padding: 0;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif;
+    padding: 0;
     line-height: 1.4;
     font-size: 16px;
     padding: 2rem;
