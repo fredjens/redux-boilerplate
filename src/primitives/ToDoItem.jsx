ac230a9992c1cd9942913b92f1c625b914d8ab0a
Fredrik Jensen <fredrik@netliferesearch.com>
2017-01-09 17:13:31 +0100

    some more styling :lipstick:

--- src/primitives/ToDoItem.jsx
+++ src/primitives/ToDoItem.jsx
@@ -4,12 +4,12 @@ import vars from '../variables';
 const fadeIn = keyframes`
   from {
     opacity: 0;
-    transform: scale(.98);
+    transform: translateX(-100%);
   }
 
   to {
     opacity: 1;
-    transform: scale(1);
+    transform: translateX(0);
   }
 `;
 
@@ -21,7 +21,7 @@ const Button = styled.li`
   margin-bottom: 1rem;
   border: 0;
   font-size: ${vars.fontSize.default};
-  animation: ${fadeIn} 300ms linear;
+  animation: ${fadeIn} 400ms;
 `;
 
 export default Button;
